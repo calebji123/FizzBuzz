e3314d26a96abbb90d25df54fb465c6ac9dfb88d
Caleb Ji <30414755+calebji123@users.noreply.github.com>
2023-09-11 09:38:22 -0400

    Made doFizzBuzz() public and added a run method with main functionality

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,11 +6,20 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 0;
         while (i < 100) {
-            i = doFizzBuzz(i);
+            doFizzBuzz(i);
+            i++;
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static void run() {
+        int i = 0;
+        while (i < 100) {
+            doFizzBuzz(i);
+            i++;
+        }
+    }
+
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -33,7 +42,6 @@ class FizzBuzz {
             System.out.println(i);
 
         }
-        i++;
-        return i;
+
     }
 }
